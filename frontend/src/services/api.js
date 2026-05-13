3f44b6c94aebca49bd3774c91575517bee7a5806
HWBAlexHC <adelgado@haverford.edu>
2026-05-13 11:32:14 -0400

    updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://cards-probability-api.onrender.com/api';
 const SESSION_ID = 'my-game-session-' + Date.now();
 
 const api = axios.create({
